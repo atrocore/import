397f83cc88372ecf847e5582d7f5ff82b68e4902
yurdm <yd@atrocore.com>
2024-04-23 11:06:51 +0300

    Add ImportFeed to the navigation on install

--- app/Event.php
+++ app/Event.php
@@ -12,6 +12,29 @@ class Event extends AfterInstallAfterDelete
         /** @var Config $config */
         $config = $this->getContainer()->get('config');
         $config->set('importJobsMaxDays', 21);
+
+        $tabList = $config->get("tabList", []);
+        if (!in_array('ImportFeed', $tabList)) {
+            $tabList[] = 'ImportFeed';
+        }
+
+        $config->set('tabList', $tabList);
+        $config->save();
+    }
+
+    public function afterDelete(): void
+    {
+        /** @var Config $config */
+        $config = $this->getContainer()->get('config');
+
+        $tabList = [];
+        foreach ($config->get("tabList", []) as $tab) {
+            if ($tab != 'ImportFeed') {
+                $tabList[] = $tab;
+            }
+        }
+
+        $config->set('tabList', $tabList);
         $config->save();
     }
 }
