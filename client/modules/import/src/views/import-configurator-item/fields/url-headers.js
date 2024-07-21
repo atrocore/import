a1bf8235c9f300680122052c16a5dd019c262291
yurdm <yd@atrocore.com>
2024-07-21 22:50:03 +0300

    Add field urlHeaders for the configurator item of url field of a File entity

---
+++ client/modules/import/src/views/import-configurator-item/fields/url-headers.js
@@ -0,0 +1,14 @@
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+Espo.define('import:views/import-configurator-item/fields/url-headers', 'views/fields/varchar', function (Dep) {
+    return Dep.extend({
+    })
+});
