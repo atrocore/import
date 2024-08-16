d5d80adcb5e183f8bad5a3b6e914e16e64ec92dc
Roman Zablodskyi <rz@atrocore.com>
2024-08-16 17:43:45 +0300

    Fixed import job acl

---
+++ client/modules/import/src/acl/import-job.js
@@ -0,0 +1,20 @@
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore UG (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+Espo.define('import:acl/import-job', 'acl', function (Dep) {
+
+    return Dep.extend({
+
+        checkModelEdit: function (model, data, precise) {
+            return Dep.prototype.checkModel.call(this, model, data, 'read', precise)
+        },
+
+    });
+});
