6ac698181406db68d029747d33287110ca759a36
Raoul Dzoukou <dzoukouraoul@gmail.com>
2025-05-29 12:26:01 +0100

    .

--- app/Jobs/ImportTypeSimple.php
+++ app/Jobs/ImportTypeSimple.php
@@ -316,7 +316,7 @@ class ImportTypeSimple extends AbstractJob implements JobInterface
                                 if (!property_exists($input, '__attributesToRemove')) {
                                     $input->__attributesToRemove = [];
                                 }
-                                $input->__attributesToRemove[] = $item['name'];
+                                $input->__attributesToRemove[] = $item['entityAttributeId'];
                             } else {
                                 $this->getService('ImportConfiguratorItem')->getFieldConverter($type)->convert($input, $item, $row);
                             }
