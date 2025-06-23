3487d118785adfbd66353964893f1210eeaf9718
Roman Ratsun <roman.ratsun@atrocore.com>
2025-06-23 11:21:12 +0000

    .

---
+++ client/modules/import/src/views/import-feed/fields/format.js
@@ -0,0 +1,23 @@
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+Espo.define('import:views/import-feed/fields/format', 'views/fields/enum',
+    Dep => Dep.extend({
+
+        afterRender() {
+            Dep.prototype.afterRender.call(this);
+
+            if (this.mode === 'detail' && this.model.get(this.name) === 'Excel') {
+                this.$el.parent().append(`<span class="text-alert" style="font-size: 12px">${this.translate('excelFormatWarning', 'labels', 'ImportFeed')}</span>`);
+            }
+        },
+
+    })
+);
