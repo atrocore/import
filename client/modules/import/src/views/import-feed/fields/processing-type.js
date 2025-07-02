2c55e2e899279059e7fccf8e33bd3730303f92f8
Roman Ratsun <roman.ratsun@atrocore.com>
2025-07-02 13:21:35 +0000

    .

--- client/modules/import/src/views/import-feed/fields/processing-type.js
+++ client/modules/import/src/views/import-feed/fields/processing-type.js
@@ -41,5 +41,48 @@ Espo.define('import:views/import-feed/fields/processing-type', 'views/fields/enu
             });
         },
 
+        initInlineEdit() {
+            Dep.prototype.initInlineEdit.call(this);
+
+            if (this.model.get(this.name) !== 'configurator') {
+                this.initShowCodeModal();
+            }
+        },
+
+        initShowCodeModal() {
+            const $cell = this.getCellElement();
+            const inlineActions = this.getInlineActionsContainer();
+
+            $cell.find('.show-code').parent().remove();
+
+            const $link = $(`<a href="javascript:" class="code-link hidden" title="${this.translate('Code')}"><i class="ph ph-code show-code"></i></a>`);
+
+            if (inlineActions.size()) {
+                inlineActions.prepend($link);
+            } else {
+                $cell.prepend($link);
+            }
+
+            $link.on('click', () => {
+                this.notify('Loading...');
+                this.createView('dialog', 'import:views/import-feed/modals/show-code', {model: this.model}, dialog => {
+                    dialog.render();
+                    this.notify(false);
+                });
+            });
+
+            $cell.on('mouseenter', e => {
+                e.stopPropagation();
+                if (this.mode === 'detail') {
+                    $link.removeClass('hidden');
+                }
+            }).on('mouseleave', e => {
+                e.stopPropagation();
+                if (this.mode === 'detail') {
+                    $link.addClass('hidden');
+                }
+            });
+        },
+
     })
 );
