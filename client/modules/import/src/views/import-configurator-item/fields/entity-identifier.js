7e3f3f647c29360c672784bdf04b9632858ae17e
yurdm <yd@atrocore.com>
2026-07-10 12:36:48 +0300

    Make entity identifier field read-only for attributes in list view

--- client/modules/import/src/views/import-configurator-item/fields/entity-identifier.js
+++ client/modules/import/src/views/import-configurator-item/fields/entity-identifier.js
@@ -14,22 +14,33 @@ Espo.define('import:views/import-configurator-item/fields/entity-identifier', 'v
         setup() {
             Dep.prototype.setup.call(this);
 
-            this.listenTo(this.model, 'change:name change:entityAttributeId', () => {
-                this.reRender();
-            });
+            this.onModelReady(() => {
+                this.listenTo(this.model, 'change:name change:entityAttributeId', () => {
+                    this.reRender();
+                });
+            })
         },
 
         afterRender() {
             Dep.prototype.afterRender.call(this);
 
-            if (this.mode !== 'list') {
+            const readOnlyTypes = ['file', 'linkMultiple', 'jsonObject'];
+
+            if (this.model.get('entityAttributeId')) {
+                this.isListView() ? this.setReadOnly() : this.hide();
+                return;
+            }
+
+            const type = this.getMetadata().get(`entityDefs.${this.model.get('entity')}.fields.${this.model.get('name')}.type`) || 'varchar';
+            if (this.isListView() && readOnlyTypes.includes(type)) {
+                this.setReadOnly();
+                return;
+            }
+
+            if (readOnlyTypes.includes(type)) {
                 this.hide();
-                if ( this.model.get('name') && !this.model.get('entityAttributeId')) {
-                    let type = this.getMetadata().get(`entityDefs.${this.model.get('entity')}.fields.${this.model.get('name')}.type`) || 'varchar';
-                    if (!['file', 'linkMultiple', 'jsonObject'].includes(type)) {
-                        this.show();
-                    }
-                }
+            } else {
+                this.show();
             }
         },
     })
